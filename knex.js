c4ff7cbad8a53276977281d5f780e5f0bc133a22
bharati7680 <bharatikudambal@gmail.com>
2023-01-25 01:50:02 +0530

    added dependencies

---
+++ knex.js
@@ -0,0 +1,12 @@
+module.exports = require("knex")({
+	client: "mysql",
+	connection: {
+		host: config.DB_HOST,
+		user: config.DB_USER,
+		password: config.DB_PASSWORD,
+		database: config.DB_NAME,
+		timezone: "IST",
+	},
+	useNullAsDefault: true,
+	acquireConnectionTimeout: 300000,
+}); 
